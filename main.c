2e344eddbae3fa0ceab3b594e762012b7096f69b
arshia-backup <160587953+arshia-backup@users.noreply.github.com>
2024-06-06 23:28:36 +0530

    guessing

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
 #include<stdio.h>
-void game_word( char word[],guess_word[])
+#include<string.h>
+void game_word( int word_len,char guess_word,char word[100])
 {
-    
+    int i;
+    for(i=0;i<word_len;i++)
+    {
+        if (guess_word==word[i])
+        {
+            printf("%c",guess_word);
+        }
+        else
+        {
+            printf("-");
+        }
+        
+    }
 
     
 }
 
 int main()
 {
-    char word[10];
-    char guess_word[10];
+    char word[100];
+    char guess_word[100];
     int word_len=strlen(word);
     int i;
 
@@ -17,10 +30,11 @@ int main()
     printf("Enter the first word for p2:");
     scanf("%s",word);
 
-    //printing --- inplace of the word
-    for(i=0;i<word_len;i++)
-    {
-        printf(" - ");
-    }
+   // system("cls");
+    printf("Enter first letter:");
+    scanf("%c",guess_word);
+    guess_word[100]=getchar();
+    game_word(word_len,guess_word,word);
+
     return 0; 
 }
